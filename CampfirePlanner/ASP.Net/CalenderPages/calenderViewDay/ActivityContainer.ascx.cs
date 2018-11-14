82a583f7dfacbac6e32fba5db252266b04896be6
Zach <s10179991@connect.np.edu.sg>
2018-11-14 12:22:39 +0800

    save

--- CampfirePlanner/ASP.Net/CalenderPages/calenderViewDay/ActivityContainer.ascx.cs
+++ CampfirePlanner/ASP.Net/CalenderPages/calenderViewDay/ActivityContainer.ascx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace CampfirePlanner.ASP.Net.CalenderPages.calenderViewDay
 {
-    public partial class calenderViewDay : System.Web.UI.Page
+    public partial class ActivityContainer : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int day = 1;
+
         }
     }
 }
