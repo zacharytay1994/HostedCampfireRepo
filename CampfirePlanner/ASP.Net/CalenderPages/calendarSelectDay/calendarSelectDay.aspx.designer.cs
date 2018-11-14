579ddd830a5ee7869be31a995425f76b27e194bb
Lau Cheng En <chengenlau4912@gmail.com>
2018-11-14 12:20:56 +0800

    Uncomment

--- CampfirePlanner/ASP.Net/CalenderPages/calendarSelectDay/calendarSelectDay.aspx.designer.cs
+++ CampfirePlanner/ASP.Net/CalenderPages/calendarSelectDay/calendarSelectDay.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace CampfirePlanner.ASP.Net.CalenderPages.calendarSelectDay {
     public partial class calendarSelectDay {
         
         /// <summary>
-        /// Calendar1 control.
+        /// calSelect control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+        protected global::System.Web.UI.WebControls.Calendar calSelect;
         
         /// <summary>
         /// Button1 control.
