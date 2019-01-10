a7897c58f43d4ad216fc4625808a4c9edad1c623
Lau Cheng En <chengenlau4912@gmail.com>
2019-01-11 00:07:57 +0800

    Implemented Up & Down Vote Functionality

--- CampfirePlanner/ASP.Net/ActivitiesPage/ActivityDesc.aspx.cs
+++ CampfirePlanner/ASP.Net/ActivitiesPage/ActivityDesc.aspx.cs
@@ -52,6 +52,14 @@ namespace CampfirePlanner.ASP.Net.ActivitiesPage
             image1.Src = result.Tables[0].Rows[0]["ImgLink"].ToString();
             //lblVotes.Text = result.Tables[0].Rows[0]["Vote"].ToString();
 
+            // Color Voted Button
+            btnUpVote.Style["background-color"] = "white";
+            btnDownVote.Style["background-color"] = "white";
+            if (voteCheck() == 1)      // Currently Up-Voted
+                btnUpVote.Style["background-color"] = "green";
+            else if (voteCheck() == 0) // Currently Down-Voted
+                btnDownVote.Style["background-color"] = "green";
+
             //Display Categories
             //lblCategories.Text = displayCategories();
         }
@@ -131,6 +139,7 @@ namespace CampfirePlanner.ASP.Net.ActivitiesPage
                 int actID = Convert.ToInt32(Request.QueryString["actID"]);
 
                 //Display Fields
+
                 string strConn = ConfigurationManager.ConnectionStrings["CampfireConnectionString"].ToString();
                 SqlConnection conn = new SqlConnection(strConn);
                 SqlCommand cmd = new SqlCommand("SELECT * FROM Images WHERE ActivityID = @actid", conn);
@@ -193,28 +202,61 @@ namespace CampfirePlanner.ASP.Net.ActivitiesPage
 
         protected void btnDownVote_Click(object sender, EventArgs e)
         {
-            if (isVoteValid())
-            {
-                string strConn = ConfigurationManager.ConnectionStrings["CampfireConnectionString"].ToString();
-                SqlConnection conn = new SqlConnection(strConn);
-                SqlCommand cmd = new SqlCommand("UPDATE Activity SET Vote = Vote-1 WHERE ActivityID = @actid", conn);
-                cmd.Parameters.AddWithValue("@actid", Convert.ToInt32(Request.QueryString["actID"]));
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                conn.Close();
-
-                showActivityDesc();
-            }
+            voteClicked(0);
         }
 
         protected void btnUpVote_Click(object sender, EventArgs e)
         {
-            if (isVoteValid())
+            voteClicked(1);
+        }
+
+        private void voteClicked(int voteStatus)
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["CampfireConnectionString"].ToString();
+
+            SqlConnection conn = new SqlConnection(strConn);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Votes WHERE ActivityID = @actID AND AccountID = @accID", conn);
+
+            cmd.Parameters.AddWithValue("@actid", Convert.ToInt32(Request.QueryString["actID"]));
+            cmd.Parameters.AddWithValue("@accID", getAccID());
+
+            SqlDataAdapter daVotes = new SqlDataAdapter(cmd);
+            DataSet result = new DataSet();
+
+            conn.Open();
+            daVotes.Fill(result, "Votes");
+            conn.Close();
+
+            if (result.Tables["Votes"].Rows.Count > 0)
             {
-                string strConn = ConfigurationManager.ConnectionStrings["CampfireConnectionString"].ToString();
-                SqlConnection conn = new SqlConnection(strConn);
-                SqlCommand cmd = new SqlCommand("UPDATE Activity SET Vote = Vote+1 WHERE ActivityID = @actid", conn);
+                int userVote = Convert.ToInt32(result.Tables[0].Rows[0]["VoteValue"]);
+                if (userVote == voteStatus) // If user clicks same vote
+                {
+                    cmd = new SqlCommand("DELETE FROM Votes WHERE ActivityID = @actID AND AccountID = @accID", conn);
+                    cmd.Parameters.AddWithValue("@accID", getAccID());
+                    cmd.Parameters.AddWithValue("@actid", Convert.ToInt32(Request.QueryString["actID"]));
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+                else // If user votes differently
+                {
+                    cmd = new SqlCommand("UPDATE Votes SET VoteValue = @vote", conn);
+                    cmd.Parameters.AddWithValue("@vote", voteStatus);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            else // If user hasn't voted before
+            {
+                cmd = new SqlCommand("INSERT INTO Votes VALUES(@accID, @actID, @vote)", conn);
+                cmd.Parameters.AddWithValue("@vote", voteStatus);
+                cmd.Parameters.AddWithValue("@accID", getAccID());
                 cmd.Parameters.AddWithValue("@actid", Convert.ToInt32(Request.QueryString["actID"]));
+
                 conn.Open();
                 cmd.ExecuteNonQuery();
                 conn.Close();
@@ -223,9 +265,50 @@ namespace CampfirePlanner.ASP.Net.ActivitiesPage
             showActivityDesc();
         }
 
-        private bool isVoteValid()
+        private int voteCheck()
         {
-            return true;
+            string strConn = ConfigurationManager.ConnectionStrings["CampfireConnectionString"].ToString();
+
+            SqlConnection conn = new SqlConnection(strConn);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Votes WHERE ActivityID = @actID AND AccountID = @accID", conn);
+
+            cmd.Parameters.AddWithValue("@actid", Convert.ToInt32(Request.QueryString["actID"]));
+            cmd.Parameters.AddWithValue("@accID", getAccID());
+
+            SqlDataAdapter daVotes = new SqlDataAdapter(cmd);
+            DataSet result = new DataSet();
+
+            conn.Open();
+            daVotes.Fill(result, "Votes");
+            conn.Close();
+
+            if (result.Tables["Votes"].Rows.Count > 0)
+            {
+                int userVote = Convert.ToInt32(result.Tables[0].Rows[0]["VoteValue"]);
+                return userVote;
+            }
+            else
+                return -1;
+        }
+
+        private int getAccID()
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["CampfireConnectionString"].ToString();
+
+            SqlConnection conn = new SqlConnection(strConn);
+            SqlCommand cmd = new SqlCommand("SELECT AccountID FROM Users WHERE Username=@name", conn);
+
+            cmd.Parameters.AddWithValue("@name", Session["UserAuthentication"].ToString());
+
+            SqlDataAdapter daID = new SqlDataAdapter(cmd);
+            DataSet result = new DataSet();
+
+            conn.Open();
+            int accID = (int)cmd.ExecuteScalar();
+            daID.Fill(result, "Users");
+            conn.Close();
+
+            return accID;
         }
     }
 }
